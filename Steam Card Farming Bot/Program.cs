16680d95faf1be8230805fcf79685aa13f5d1d88
XR069 | HAWGT <r4th4ck3r@gmail.com>
2015-06-22 16:35:08 +0100

    1.0
    Uploaded

---
+++ Steam Card Farming Bot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam_Card_Farming_Bot
+{
+    using System;
+    using System.Runtime.InteropServices;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("***Steam Card Farming Bot***");
+            System.Console.WriteLine("Program created by: XRKT");
+            System.Console.WriteLine("Steam URL: http://steamcommunity.com/profiles/76561198017577802");
+            System.Console.WriteLine("*******************************************************************");
+            System.Console.WriteLine("___________________________________________________________________");
+            System.Console.WriteLine("The program is now active, press any key to close when you're done.");
+            System.Console.WriteLine("___________________________________________________________________");
+            Steam.Init();
+            System.Console.WriteLine("___________________________________________________________________");
+            Console.ReadKey();
+            Steam.Kill();
+
+        }
+    }
+
+    public class Steam
+    {
+        public static bool SteamInit;
+
+        public static void Init()
+        {
+            SteamInit = SteamAPI_Init();
+        }
+
+        public static void Kill()
+        {
+            SteamAPI_Shutdown();
+        }
+
+        [DllImport("steam_api.dll")]
+        private static extern bool SteamAPI_Init();
+        [DllImport("steam_api.dll")]
+        private static extern bool SteamAPI_Shutdown();
+    }
+}
---
+++ Steam Card Farming Bot/ReadME.txt
@@ -0,0 +1,15 @@
+Steam Card Farming Bot
+Made by XRKT
+Steam URL: http://steamcommunity.com/profiles/76561198017577802
+
+
+
+**How to use**
+
+Search the game you have and want to farm cards on Steam Store
+
+You will have something like this:
+
+http://store.steampowered.com/app/730/ (Counter-Strike: Global Offensive)
+
+The app number for this game is 730, so go ahead and edit steam_appid text file and put that number there, this only works for games that you own.
